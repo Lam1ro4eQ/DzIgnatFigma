3fa9679376a6bdc264d5f6c2a6080fe2dc9b7552
Lam1ro4eQ <alsajgushev@gmail.com>
2022-11-19 21:36:27 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,14 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
             switch (action.payload) {
                 case "up": {
-                    const collatore = new Intl.Collator
-                    let sortState = [...state].sort((x, y) => collatore.compare(x.name, y.name))
-                    return sortState // need to fix
+                    // const collatore = new Intl.Collator
+                    // let sortState = [...state].sort((x, y) => collatore.compare(x.name, y.name))
+                    // return sortState // need to fix
+                    const newState = [...state].sort((a,b) => {
+                        if (a.name > b.name) return 1
+                        else if (a.name < b.name) return -1
+                        else return 0
+                    })
+                    return newState
                 }
                 case "down": {
                     const collatore = new Intl.Collator
